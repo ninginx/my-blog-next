1c88351b857247aff61d3748be378c421c3156fb
ninginx <publictaro@gmail.com>
2021-05-06 14:16:38 +0900

    ogp description fixed

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -32,10 +32,7 @@ const Meta: VFC = () => (
     <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
     <meta name="theme-color" content="#000" />
     <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-    <meta
-      name="description"
-      content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-    />
+    <meta name="description" content="ブログや" />
     <meta property="og:image" content={HOME_OG_IMAGE_URL} />
   </Head>
 );
