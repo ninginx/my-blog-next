2c4aa1c3bca69c0c61561dad3f7cf6b750d37fb6
ninginx <publictaro@gmail.com>
2021-05-06 12:00:18 +0900

    sns icon added

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -7,15 +7,31 @@ type Props = {
 
 const Avatar: VFC<Props> = ({ name, picture }: Props) => (
   <div className="flex items-center mb-16">
-    <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+    <img src={picture} className="w-24 h-24 rounded-full mr-4" alt={name} />
     <div className="flex flex-col">
-      <div className="text-xl">{name} : メーカー勤務のWebエンジニア</div>
-      <div className="text-xl">
+      <div className="text-xl mb-4">
+        {name} : メーカー勤務のWebエンジニア。人生がエモい。
+      </div>
+      <div className="text-xl flex">
         <a
-          className="underline text-blue-600"
+          className="underline text-blue-600 mr-4"
           href="https://twitter.com/tarokov"
         >
-          twitter
+          <img
+            src="/assets/blog/logo/twitter-logo.png"
+            className="w-10 "
+            alt="twitter-link"
+          />
+        </a>
+        <a
+          className="underline text-blue-600"
+          href="https://github.com/ninginx"
+        >
+          <img
+            src="/assets/blog/logo/github-logo.png"
+            className="w-10 "
+            alt="twitter-link"
+          />
         </a>
       </div>
     </div>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,7 +3,7 @@ import { VFC } from "react";
 const Intro: VFC = () => (
   <section className="flex-col flex items-center mt-16 mb-16 md:mb-12">
     <h3 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:px-8 text-center">
-      喜怒哀楽楽と生きている
+      喜怒哀楽楽 v2!!
     </h3>
   </section>
 );
